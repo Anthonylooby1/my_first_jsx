874253861c0e581e635a412d9b505571800da058
Anthonylooby1 <anthonylooby@yahoo.com>
2023-08-02 17:36:12 -0400

    putting it together

--- src/components/firstcomponent.jsx
+++ src/components/firstcomponent.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 const PersonCard = (props) => {
     const {firstName, lastName, age, hairColor} = props
+    const[ageActual, setAgeActual] = useState(age)
+    const haveBirthday = ()=> {
+        setAgeActual(ageActual + 1)
+    }
     return(
         <div>
-            <h1 style={{color:"red"}}>Instructor {firstName}</h1>
-            <p>Age: {age}</p>
+            <h1 style={{color:"green"}}>Instructor {firstName}</h1>
+            <p>Age: {ageActual}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={haveBirthday}>Happy Birthday {firstName}</button>
         </div>
     );
 }
